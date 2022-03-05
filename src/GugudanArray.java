bb3ff8414a4713db60ee17c5649c5a085c942e6f
Tae Young Ryu <yty0819@gmail.com>
2022-03-06 01:46:45 +0900

    #7 - Use Class

--- src/GugudanArray.java
+++ src/GugudanArray.java
@@ -1,15 +1,15 @@
 public class GugudanArray {
-    public static void main(String [] args){
-        int[] times = new int[9];
-        for (int j = 2; j < 10; j++) {
-            for(int i = 0 ; i < times.length; i++) {
-                times[i] = j*(i+1);
-            }
-
-            for (int i = 0; i < times.length; i ++) {
-                System.out.println(times[i]);
-            }
-
-        }
-    }
+//    public static void main(String [] args){
+//        int[] times = new int[9];
+//        for (int j = 2; j < 10; j++) {
+//            for(int i = 0 ; i < times.length; i++) {
+//                times[i] = j*(i+1);
+//            }
+//
+//            for (int i = 0; i < times.length; i ++) {
+//                System.out.println(times[i]);
+//            }
+//
+//        }
+//    }
 }
--- src/GugudanMethod.java
+++ src/GugudanMethod.java
@@ -1,12 +1,4 @@
 public class GugudanMethod {
-    public static void main(String[] args){
-        for(int i = 2; i<10; i++){
-            int[] result = calculate(i);
-            print(result);
-        }
-
-
-    }
     public static int[] calculate(int times){
         int[] result = new int[9];
         for(int i = 0 ; i < result.length; i++) {
